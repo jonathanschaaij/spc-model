0cba7132dcdf01337a176b5d3792588330037746
Jonathan Schaaij <jonathan.schaaij@gmail.com>
2023-01-06 15:33:57 +0100

    First running model

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repository contains a simulink model of a self-propelled-caravan (SPC), an electric caravan designed to extend the range of an EV towing the caravan. 
 
 ## How to run the model
-TODO
-test here
+Open run_model.m as well as model. 
+First set the workspace variables by launching run_model.m. Then the simulink model should work 
 
 ## 
--- run_model.m
+++ run_model.m
@@ -1,5 +1,36 @@
+% Author: Tjeerd Bakker, Jonathan Schaaij
+clear;
 close all;
 clc;
 
 
-plot(GNSS.time - GNSS.time(2), GNSS.vel * 3.6)
+%% Constant
+airDensity = 1.293;% kg/m^3
+gravity = 9.81; %m/s^2
+
+%% Design tunable
+aeroDragCoeff = 0.2;
+rollingResistCoeff = 0.01; % TODO: determine source
+frontArea = 4; %m^2
+
+massBatt = 500; % kg
+massCaravan = 500; % kg
+massStorage = 500; % kg
+
+nTires = 2;
+massTire = 7; %kg
+radiusTire = 0.3; %m    
+
+%% Trip 
+slope = 0; %radian
+
+%% Equations
+
+inertia_tire = 0.5 * massTire * radiusTire^2;
+massSPC = massBatt + massCaravan + massStorage + nTires * massTire; %kg
+
+
+inertialMassSPC = massSPC + nTires * inertia_tire / radiusTire^2;
+
+% TODO: center of mass on axis of car, behind, in front. Changes the
+% rolling resistance. 
